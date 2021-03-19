f63b50a6f4774f8224909ca667613f3bd1f8b28d
guzvladimir <vova1195@yandex.ru>
2021-03-19 16:32:26 +0300

    Changes test_task02 homework_4

--- homework_4/tests/test_task_2_mock_input.py
+++ homework_4/tests/test_task_2_mock_input.py
@@ -4,20 +4,22 @@ import pytest
 from task02.task_2_mock_input import count_dots_on_i
 
 
-@patch("urllib.request.urlopen")
+# эксперимент
+@patch(
+    "urllib.request.urlopen",
+    return_value=Mock(read=Mock(return_value=b"illustrative example")),
+)
 def test_count_dots_on_i(mock_urlopen):
-    mock = Mock()
-    mock.read.return_value = b"illustrative example"
-    mock_urlopen.return_value = mock
-    result = count_dots_on_i(mock)
+    result = count_dots_on_i(mock_urlopen)
     assert result == 2
 
 
-@patch("urllib.request.urlopen")
+@patch(
+    "urllib.request.urlopen",
+    return_value=Mock(
+        read=Mock(return_value=b"https://example.com/", side_effect=ValueError)
+    ),
+)
 def test_count_dots_on_i_error(mock_urlopen):
-    mock = Mock()
-    mock.read.return_value = b"https://example.com/"
-    mock_urlopen.return_value = mock
-    mock_urlopen.side_effect = ValueError
     with pytest.raises(ValueError):
-        count_dots_on_i(mock)
+        count_dots_on_i(mock_urlopen)
