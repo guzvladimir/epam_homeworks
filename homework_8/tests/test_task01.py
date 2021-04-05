7d7d3932b302eed0735f8eecbc033d0c12131bb7
guzvladimir <vova1195@yandex.ru>
2021-04-05 14:43:40 +0300

    Task01 and test homework_8 renamed

--- homework_8/tests/test_task01.py
+++ homework_8/tests/test_task01.py
@@ -2,8 +2,8 @@ import pytest
 from task01.task01 import KeyValueStorage
 
 
-def test_keyvaluestorage():
-    storage = KeyValueStorage("task1.txt")
+def test_key_value_storage():
+    storage = KeyValueStorage("test_task01_key_value_storage.txt")
     assert storage["name"] == storage.name == "kek"
     assert storage.song == "shadilay"
 
@@ -12,4 +12,4 @@ def test_value_error():
     with pytest.raises(
         ValueError, match="This key cannot be assigned to an attribute."
     ):
-        KeyValueStorage("test_task01_negative.txt")
+        KeyValueStorage("test_task01_value_error.txt")
